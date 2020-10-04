2aa0bec646e92963ea1fbf33db2cc68d58b181b5
joshua.c.terrell@gmail.com <joshua.c.terrell@gmail.com>
2020-10-04 17:17:29 -0400

    -

--- percentallocations.js
+++ percentallocations.js
@@ -1,12 +1,3 @@
-var preretire_stockallocation = 70;
-var preretire_tbondallocation = 10;
-var preretire_corpbondallocation = 20;
-var preretire_cashallocation = 0;
-
-var retire_stockallocation = 50;
-var retire_tbondallocation = 20;
-var retire_corpbondallocation = 20;
-var retire_cashallocation = 10;
 
 Highcharts.chart('dashboard-percallocations-preretire', {
     colors: ['#2E93fA', '#66DA26', '#546E7A', '#E91E63', '#FF9800', '#00FFF5', '#9F00FF'],
@@ -34,7 +25,7 @@ Highcharts.chart('dashboard-percallocations-preretire', {
         type: 'pie'
     },
     title: {
-        text: null
+        text: 'Starting Fund Allocation'
     },
     tooltip: {
         pointFormat: '{series.name}: <b>{point.percentage:.0f}%</b>'
@@ -71,6 +62,76 @@ Highcharts.chart('dashboard-percallocations-preretire', {
         }, {
             name: 'Cash',
             y: preretire_cashallocation
+        }]
+    }]
+});
+
+Highcharts.chart('dashboard-percallocations-preretire', {
+    colors: ['#2E93fA', '#66DA26', '#546E7A', '#E91E63', '#FF9800', '#00FFF5', '#9F00FF'],
+    credits: {
+        enabled: false
+    },
+    exporting: {
+        enabled: true,
+        scale: 6,
+        buttons: {
+            contextButton: {
+                menuItems: ["downloadPNG", "downloadJPEG", "downloadPDF", "downloadSVG", "separator", "viewFullscreen", "printChart", 'downloadCSV', 'downloadXLS']
+            }
+        }
+    },
+    boost: {
+        enabled: true,
+        useGPUTranslations: true,
+        usePreallocated: true
+    },
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Final Fund Allocation'
+    },
+    subtitle: {
+        text: 'Adjusted from '+age_to_start_adjust_port_allocations+ ' yrs old to '+age_to_end_adjust_port_allocations+'yrs old.'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.0f}%</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.0f} %'
+            }
+        }
+    },
+    series: [{
+        name: 'Allocation',
+        colorByPoint: true,
+        data: [{
+            name: 'Stock',
+            y: retire_stockallocation,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Treasury Bonds',
+            y: retire_tbondallocation
+        }, {
+            name: 'Corporate Bonds',
+            y: retire_corpbondallocation
+        }, {
+            name: 'Cash',
+            y: retire_cashallocation
         }]
     }]
 });
