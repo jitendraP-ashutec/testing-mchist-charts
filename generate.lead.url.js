36a4ec8993f8216ba928a476e36380069d03f473
jct <joshua.c.terrell@gmail.com>
2020-10-09 09:07:16 -0400

    Added field to generate.lead.url.js for compact form option checkbox.

--- generate.lead.url.js
+++ generate.lead.url.js
@@ -1,21 +1,24 @@
 
 // Takes an email address (raw), form type, and form enable/disable input (1=enabled)
 // and generates a URL for that form (to cloudflare worker)
-function getLeadEmbedCode(email, formnum, formenable) {
+function getLeadEmbedCode(email, formnum, formenable, formbigsmall) {
 
     const btoa_emailaddresstosendto = getrandomchar_forlead(3) + btoa(email.trim()) + getrandomchar_forlead(2);
 
     // Input Error Checks
-    if (isNaN(formenable)) { formenable = 1; }
+
+    if (isNaN(formenable)) { formenable = 1; } // Is the form enabled or not (show or hide form)
     if (formenable < 0 || formenable > 1) { formenable = 1; }
 
-    if (isNaN(formnum)) { formnum = 0; }
+    if (isNaN(formnum)) { formnum = 0; } // form selection #
     if (formnum < 0 || formnum > 30) { formnum = 0; }
 
+    if (isNaN(formbigsmall)) { formbigsmall = 0; } // 0 = big, 1 = small (compact)
+    if (formbigsmall< 0 || formbigsmall > 1) { formbigsmall = 0; }
 
-    // Example: https://widgets.eadvisor.app/getform/?email=aaaam9zaHVhLmMudGVycmVsbEBnbWFpbC5jb20=aa&formnum=1&formenable=1
+    // Example: https://widgets.eadvisor.app/getform/?email=aaaam9zaHVhLmMudGVycmVsbEBnbWFpbC5jb20=aa&formnum=1&formenable=1&formbigsmall=1
     const URLReturn = "https://widgets.eadvisor.app/getform/?email=" + btoa_emailaddresstosendto
-        + "&formnum=" + formnum + "&formenable=" + formenable;
+        + "&formnum=" + formnum + "&formenable=" + formenable + "&formbs=" + formbigsmall;
 
 
     const EmbedCodeReturn = `<iframe width='100%' height='100%' style='border:none;' src='${URLReturn}'></iframe>`;
